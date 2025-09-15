ebaa3ab2dea5ba4e1f1aaf97d2715b3deb2e0140
tapascod315 <tapascod315@gmail.com>
2025-09-14 21:54:31 -0500

    Despliegue 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatesModule } from './dates/dates.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [DatesModule],
+  controllers: [AppController], // aquí agregamos la ruta raíz
 })
 export class AppModule {}
 
